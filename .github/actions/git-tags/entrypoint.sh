eda9f7551a16814cf6a03ce98ad25b547b02a612
gabeduke <gabeduke@gmail.com>
2019-09-15 10:56:08 -0400

    fix data bloc

--- .github/actions/git-tags/entrypoint.sh
+++ .github/actions/git-tags/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -l
 
 TAG=$(cat /github/workspace/.release)
+DATA='{"ref": "refs/tags/'"$TAG"'","sha": "'"$GITHUB_SHA"'"}'
 
 if [ -z "$TAG" ]
 then
     echo "Release not found.."
 else
     echo "Tag: $TAG"
+    echo "DATA: ${DATA}"
 
     # POST a new ref to repo via Github API
     curl -s -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs \
     -H "Authorization: token $GITHUB_TOKEN" \
-    -d '{"ref": "refs/tags/'"$TAG"'","sha": '"$GITHUB_SHA"'"}'
+    -d ${DATA}
 fi
